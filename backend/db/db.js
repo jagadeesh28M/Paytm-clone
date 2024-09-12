26925dadd9253f9d2de366d5ae026a8d3e34126c
Jagadeesh <jagadeesh28.m@gmail.com>
2024-09-12 11:22:27 +0530

    added accountSchema

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,44 +1,52 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://jagadeesh28:pass@cluster0.f5bsn.mongodb.net/paytm")
-const userSchema = new mongoose.Schema({
-    username : String,
-    firstname : String,
-    lastname : String,
-    password : String
-})
 
-const User = mongoose.model('User',userSchema);
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
-    User
+	User,
+  Account,
 };
-
-
-// const userSchema = new mongoose.Schema({
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//         trim: true,
-//         lowercase: true,
-//         minLength: 3,
-//         maxLength: 30
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         minLength: 6
-//     },
-//     firstName: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         maxLength: 50
-//     },
-//     lastName: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         maxLength: 50
-//     }
-// });
